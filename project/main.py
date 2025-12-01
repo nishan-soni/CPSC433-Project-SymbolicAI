8f27fde0d00ed029aeecf1526eca45031487779a
Nishan Soni <nishan.soni2@gmail.com>
2025-12-01 14:18:48 -0700

    read me

--- project/main.py
+++ project/main.py
@@ -7,7 +7,12 @@ import tracemalloc
 def main():
     input_data = get_input_data(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7], sys.argv[8], sys.argv[9])
 
-    search = AndTreeSearch(input_data, break_limit=1)
+    if len(sys.argv) > 10:
+        break_limit = 1
+    else:
+        break_limit = None
+
+    search = AndTreeSearch(input_data, break_limit=break_limit)
     search.search()
     print(search.get_formatted_answer_with_eval())
 
